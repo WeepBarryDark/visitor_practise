2e38cfe9de98c6e3c14fe8b2098b21a11f1c8394
Barry Wang <weepbarryphotograph@gmail.com>
2026-01-27 18:10:22 +1100

    all upload

--- lib/pages/kiosk_visitor_sign_out/views/kiosk_visitor_sign_out_page.dart
+++ lib/pages/kiosk_visitor_sign_out/views/kiosk_visitor_sign_out_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:visitor_practise/core/responsive/aap_breakpoints.dart';
+import 'package:visitor_practise/pages/kiosk_visitor_sign_out/controllers/kiosk_visitor_sign_out_controller.dart';
+import 'package:visitor_practise/pages/kiosk_visitor_sign_out/widgets/kiosk_visitor_sign_out_main.dart';
+import 'package:visitor_practise/shared_widgets/parent_widgets/background_image_parent.dart';
+import 'package:visitor_practise/shared_widgets/parent_widgets/kiosk_guard_parent.dart';
+import 'package:visitor_practise/shared_widgets/parent_widgets/loading_circle_interface.dart';
 
 class KioskVisitorSignOutPage extends StatefulWidget {
   const KioskVisitorSignOutPage({super.key});
@@ -8,8 +14,27 @@ class KioskVisitorSignOutPage extends StatefulWidget {
 }
 
 class _KioskVisitorSignOutPageState extends State<KioskVisitorSignOutPage> {
+  late final KioskVisitorSignOutController _kioskVisitorSignOutController;
+
+  @override
+  void initState() {
+    super.initState();
+    _kioskVisitorSignOutController = KioskVisitorSignOutController();
+  }
+
+ 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final width = MediaQuery.of(context).size.width;
+    final maxBodyWidth = AppBreakpoints.getContentWidth(width);
+
+    if (!_kioskVisitorSignOutController.isCheckingInitial) {
+      return const LoadingCircleInterface();
+    }
+    
+    return BackgroundImageParent(
+       customBackgroundUrl: _kioskVisitorSignOutController.backgroundImageUrl,
+       mainWidget: KioskGuardParent(child:KioskVisitorSignOutMain(kioskVisitorSignOutController: _kioskVisitorSignOutController,maxBodyWidth:maxBodyWidth)),
+    );
   }
 }
